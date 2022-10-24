79a31258df28cf1d812e83a9924e24dc1871e0fd
Ezra Chung <ezra.chung97@gmail.com>
2022-10-24 13:14:21 -0700

    Created post request for create_cupcakes

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
+
 @app.get("/api/cupcakes")
 def list_all_cupcake():
     """Return JSON {cupcakes: [{id, flavor, size, rating, image}...]} """
@@ -20,6 +21,7 @@ def list_all_cupcake():
     serialized = [c.serialize() for c in cupcakes]
 
     return jsonify(cupcakes=serialized)
+
 
 @app.get('/api/cupcakes/<cupcake_id>')
 def show_single_cupcake_detail(cupcake_id):
@@ -33,3 +35,20 @@ def show_single_cupcake_detail(cupcake_id):
     return jsonify(cupcake=serialized)
 
 
+@app.post('/api/cupcakes')
+def create_cupcake():
+    """ Creates cupcake and returns the JSON of cupcake """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
